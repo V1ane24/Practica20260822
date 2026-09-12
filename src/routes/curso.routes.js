6674ddf50fa49891f005ce92173ab0605b3f7451
V1ane24 <cv25001@esfe.agape.edu.sv>
2026-09-11 19:20:11 -0600

    Cambios para la Practica 20260910

--- src/routes/curso.routes.js
+++ src/routes/curso.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const cursoController = require('../controllers/curso.controller');
+const { verificarToken, permitirRoles } = require('../middlewares/auth.middleware');
+
+router.use(verificarToken, permitirRoles('ADMIN', 'MANTENIMIENTO'));
 
 router.post('/', cursoController.crear);
 router.get('/', cursoController.obtenerTodos);
